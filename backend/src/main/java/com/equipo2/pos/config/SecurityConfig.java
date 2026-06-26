a030b9e7a5f246969b243ef0fe1156af5d5f56a4
ErickMD23 <erick_karmine@icloud.com>
2026-06-26 07:17:00 -0600

    fix cors vercel

--- backend/src/main/java/com/equipo2/pos/config/SecurityConfig.java
+++ backend/src/main/java/com/equipo2/pos/config/SecurityConfig.java
@@ -34,10 +34,15 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of(
+        "http://localhost:4200",
+        "https://punto-venta-equipo2-v.vercel.app"
+));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true);
+
+config.setAllowedHeaders(List.of("*"));
+
+config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
